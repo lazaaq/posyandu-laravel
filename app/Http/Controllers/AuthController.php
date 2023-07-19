8d5ceffc6628f39b04bb6c1502c9f0c2a87aea70
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-07-19 07:57:44 +0700

    fix: login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,4 +53,13 @@ class AuthController extends Controller
 
         return response()->json(['message' => 'Successfully logged out']);
     }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
 }
