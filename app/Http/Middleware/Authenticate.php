5e2e169a1d12508d96aab3466d488843763565a1
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-07-22 01:55:38 +0700

    faet: handle api error response

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,6 +3,8 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
@@ -17,5 +19,13 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+    
+    public function handle($request, Closure $next, ...$guards) {
+        if (!Auth::guard('api')->check()) {
+            return responseAPI(401, 'Unauthorized', null);
+        }
+
+        return $next($request);
     }
 }
