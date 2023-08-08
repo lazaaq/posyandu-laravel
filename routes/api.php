90a61755f3aa0b989ac51911327f63396ea11581
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-08-08 23:09:29 +0700

    feat: add few endpoint for kelurahan api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChildrenController;
 use App\Http\Controllers\DataCollectionController;
 use App\Http\Controllers\FolderController;
+use App\Http\Controllers\KelurahanController;
 use App\Http\Controllers\PosyanduController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -30,22 +31,21 @@ Route::group(['middleware' => 'api', 'prefix' => 'posyandu'], function ($router)
     Route::get('{id}', [PosyanduController::class, 'show'])->middleware('role:kelurahan,posyandu');
     Route::post('store', [PosyanduController::class, 'store'])->middleware('role:kelurahan');
     Route::delete('{id}', [PosyanduController::class, 'destroy'])->middleware('role:kelurahan');
+    Route::get('get_username/{posyandu_id}', [PosyanduController::class, 'get_username'])->middleware('role:kelurahan');
     Route::post('reset_password', [PosyanduController::class, 'reset_password'])->middleware('role:kelurahan');
     Route::get('settings/{posyandu_id}', [PosyanduController::class, 'settings'])->middleware('role:kelurahan');
 });
 
 Route::group(['middleware' => 'api', 'prefix' => 'folder'], function ($router) {
-    Route::get('', [FolderController::class, 'index'])->middleware('role:kelurahan');
+    Route::get('all_based_posyandu/{posyandu_id}', [FolderController::class, 'all_based_posyandu'])->middleware('role:kelurahan');
     Route::get('{id}', [FolderController::class, 'show'])->middleware('role:kelurahan,posyandu');
     Route::post('store', [FolderController::class, 'store'])->middleware('role:posyandu');
 });
 
 Route::group(['middleware' => 'api', 'prefix' => 'data'], function ($router) {
-    Route::get('based_folder/{folder_id}', [DataCollectionController::class, 'based_folder'])->middleware('role:kelurahan');
-    Route::get('based_posyandu/{posyandu_id}', [DataCollectionController::class, 'based_posyandu'])->middleware('role:kelurahan');
-    Route::get('based_children/{children_id}', [DataCollectionController::class, 'based_children'])->middleware('role:kelurahan');
+    Route::get('{id}', [DataCollectionController::class, 'show'])->middleware('role:kelurahan');
     Route::post('store', [DataCollectionController::class, 'store'])->middleware('role:posyandu');
-    Route::get('history/{children_id}', [DataCollectionController::class, 'history'])->middleware('role:posyandu');
+    Route::get('history/{children_id}', [DataCollectionController::class, 'history'])->middleware('role:kelurahan,posyandu');
     Route::delete('{data_id}', [DataCollectionController::class, 'destroy'])->middleware('role:posyandu');
 });
 
@@ -57,4 +57,8 @@ Route::group(['middleware' => 'api', 'prefix' => 'children'], function ($router)
     Route::put('{id}', [ChildrenController::class, 'update'])->middleware('role:posyandu');
     Route::delete('{id}', [ChildrenController::class, 'destroy'])->middleware('role:posyandu');
     Route::get('based_posyandu/{posyandu_id}', [ChildrenController::class, 'based_posyandu'])->middleware('role:kelurahan');
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'kelurahan'], function ($router) {
+    Route::get('show', [KelurahanController::class, 'show'])->middleware('role:kelurahan');
 });
