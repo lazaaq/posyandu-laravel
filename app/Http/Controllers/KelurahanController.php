8d793f188f831408a46056b476e7f852d2198471
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2023-08-20 16:57:56 +0700

    fix: kelurahan controller

--- app/Http/Controllers/KelurahanController.php
+++ app/Http/Controllers/KelurahanController.php
@@ -62,11 +62,11 @@ class KelurahanController extends Controller
             $childrencount = $children->count();
             foreach($posyandus as $posyandu) {
                 if($posyandu['folders']->count() == 0) {
-                    $posyandu['folder_terbaru'] = null;
+                    $posyandu['folder_terbaru'] = [];
                     continue;
                 }
                 $folder = $posyandu['folders']->sortByDesc('created_at')->take(1);
-                $posyandu['folder_terbaru'] = $folder->setVisible(['nama', 'tanggal'])->toArray();
+                $posyandu['folder_terbaru'] = $folder->setVisible(['nama', 'tanggal']);
                 $posyandu->makeHidden('folders')->toArray();
             }
             $ringkasanKategori = [
